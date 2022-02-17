c13849d9404c37381210dbdfa98f3824de13e15a
César Mâncio Silva <95443273+CesarMancioSilva@users.noreply.github.com>
2022-02-17 17:21:27 -0300

    phpito

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+echo "Lojinha de leite fermentado PHPito";
+?>
+
+<br>
+
+<?php
+echo "Ano: ";
+echo 2022;
+?>
+
+<br>
+
+<?php
+echo "Preço promocional R$";
+echo 1.4;
+?>
+
+<br>
+
+<?php
+echo "Estoque inicial: ";
+$estoque = 1701;
+echo $estoque;
+?>
+
+<br>
+
+<?php
+echo "Saída 17";
+?>
+
+<br>
+
+<?php
+echo "Estoque atual: ";
+$estoque = 1701 -17;
+echo $estoque;
+?>
+
+<br> 
+
+<?php
+echo "Entrada: 2";
+?>
+
+<br> 
+
+<?php
+echo "Estoque atual: ";
+echo $estoque +2;
+?>
